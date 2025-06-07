f485aeda3248f97bedcf53aa9b3681bb5ad571af
jonah odoh <jonahodoh@gmail.com>
2025-06-07 11:56:29 +0100

    my first commit for this project

---
+++ src/main/java/brightlogic/globapay/GlobapayApplication.java
@@ -0,0 +1,13 @@
+package brightlogic.globapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobapayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobapayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brightlogic/globapay/GlobapayApplicationTests.java
@@ -0,0 +1,13 @@
+package brightlogic.globapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobapayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
